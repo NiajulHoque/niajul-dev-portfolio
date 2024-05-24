3129efaec6825365719ba03a32f06c9e52c8be66
Niajul Hoque <nh98.programmer@gmail.com>
2024-05-24 07:38:48 +0100

    chore: [86byx6tta] - Set viewport to match all device screen sizes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,20 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./index.css";
+import NavbarSection from "./components/core/NavbarSection";
+import ContactSection from "./components/ContactSection";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Niajul Hoque",
   description: "My personal developer portfolio",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+  maximumScale: 1.0,
 };
 
 export default function RootLayout({
@@ -16,7 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex h-full flex-col justify-between">
+          <NavbarSection />
+          {children}
+          <ContactSection />
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
 import AboutMeSection from "./components/AboutMeSection";
-import ContactSection from "./components/ContactSection";
 import EducationSection from "./components/EducationSection";
 import DashboardSection from "./components/DashboardSection";
 import ProjectsSection from "./components/ProjectsSection";
 import WorkExperienceSection from "./components/WorkExperienceSection";
-import NavbarSection from "./components/core/NavbarSection";
 
 function Home() {
   return (
     <div>
-      <NavbarSection />
       <DashboardSection />
       <AboutMeSection />
       <WorkExperienceSection />
       <ProjectsSection />
       <EducationSection />
-      <ContactSection />
     </div>
   );
 }
